8a3f937250a871f3061dbc279f29f5af0dfd9cd0
Tasluf665 <tasluf665@gmail.com>
2026-04-29 14:59:59 +0300

    cc

--- jobflow-backend/src/app.js
+++ jobflow-backend/src/app.js
@@ -10,8 +10,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: '*',
 }));
 
 // Body parser
